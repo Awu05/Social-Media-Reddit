8537c502fc750d5973bf7dfc5f816b6b38fa340a
Steven <slyktey@gmail.com>
2016-04-13 19:01:54 -0400

    Bunch of data stuff

--- MiningReddit.py
+++ MiningReddit.py
@@ -24,18 +24,31 @@ for subreddit in subredditList:
 user_agent = "Testing PRAW API /u/MiningReddit"
 r = praw.Reddit(user_agent=user_agent)
 
-'''
+
 #Get top stories from front page of reddit
 #Idk why I found this
 for submission in r.get_front_page(limit=10):
     print submission.title
-'''
+    print "Username of author: ", submission.author
+    user_name = submission.author
+    user = r.get_redditor(user_name)
+
+    thing_limit = 10
+    gen = user.get_submitted(limit=thing_limit)
+    karma_by_subreddit = {}
+    for thing in gen:
+        subreddit = thing.subreddit.display_name
+        karma_by_subreddit[subreddit] = (karma_by_subreddit.get(subreddit, 0)
+                                         + thing.score)
+    import pprint
+    pprint.pprint(karma_by_subreddit)
+    print "\n"
 
 
 
 '''
 #Gets the amount of karma from the specific subreddit they got it from
-user_name = "AteMyWheaties" #Random User
+user_name = "painmatrix" #Random User
 user = r.get_redditor(user_name)
 
 thing_limit = 10
@@ -48,3 +61,16 @@ for thing in gen:
 import pprint
 pprint.pprint(karma_by_subreddit)
 '''
+
+'''
+user = r.get_redditor('kayjay25')
+submissions = user.get_submitted()
+
+self_texts = []
+for link in submissions:
+    self_texts.append(link.selftext)
+    print link
+'''
+
+
+
