c67bdf9d62c9bef848dc2c77dc4867b616dd12d7
Steven <slyktey@gmail.com>
2016-04-14 19:24:41 -0400

    Still getting there...

--- MiningReddit.py
+++ MiningReddit.py
@@ -2,18 +2,19 @@ import praw
 import webbrowser
 
 
-'''
+
 r = praw.Reddit(user_agent="test")
 
+'''
 #subredditList = r.get_popular_subreddits(limit=20)
 i = 0
 while(i < 100):
     subredditList = r.get_random_subreddit()
     print subredditList
     i += 1
-'''
 
-'''
+
+
 for subreddit in subredditList:
     print subreddit
 '''
@@ -44,8 +45,10 @@ for submission in r.get_front_page(limit=10):
                                              + thing.score)
         for key, value in sorted(karma_by_subreddit.iteritems(), key=lambda (k,v): (v,k)):
             print "%s: %s" % (key, value)
-        print key
-        print r.get_subreddit_recommendations(key, omit=None), "\n" 
+        print "\n"
+        print user_name, "subreddit with most karma from submissions: r/", key, "\n"
+        print r.get_subreddit_recommendations(key, omit=None), "\n"
+        print "------------------------------------------------------ \n"
         
     except Exception:
         print "User's Profile is private.  Cannot access data \n \n"
@@ -74,14 +77,14 @@ pprint.pprint(karma_by_subreddit)
 '''
 
 '''
-user = r.get_redditor('kayjay25')
+user = r.get_redditor('99yankee')
 submissions = user.get_submitted()
 
 self_texts = []
 for link in submissions:
     self_texts.append(link.selftext)
     print link
+
+
 '''
-
-
 
