dba5eb2ac500e1ec61422f380cd34be26beeb9ba
Awu05 <andywu.05@gmail.com>
2016-04-23 22:39:34 -0400

    Added a Hint Function and fixed some stuff

--- Subreddit_Game.py
+++ Subreddit_Game.py
@@ -29,7 +29,8 @@ class subredditGame:
         while current != end:
             print "Your CURRENT Subreddit is:", current
             print "Your ENDING Subreddit is:", end, "\n"
-            print 'Hint: You can type "Repick" to change your \ncurrent subreddit if you are stuck or if \nno available subreddits are shown.\n'
+            print 'Hint 1: You can type "Repick" to change your \ncurrent subreddit if you are stuck or if \nno available subreddits are shown.\n'
+            print '\nHint 2: You can type "Hint" to get a list of \nsubreddits related to the ending subreddit.\n'
             
             recom = str(r.get_subreddit_recommendations(current, omit=None))
             recomlen = len(r.get_subreddit_recommendations(current, omit=None))
@@ -44,17 +45,19 @@ class subredditGame:
                 while start == end:
                     current = str(r.get_random_subreddit())
                     
-                #current = start
+                
                 print "Your NEW CURRENT Subreddit is:", current, "\n"
+                print "Your ENDING Subreddit is:", end, "\n"
 
-            if(recomlen2 < 2):
+            if(recomlen2 < 4):
                 print "Repicking ENDING subreddit due to lack of choices.\n"
                 end = str(r.get_random_subreddit())
                 while start == end:
                     end = str(r.get_random_subreddit())
 
-                current = start
-                print "Your NEW ENDING Subreddit is:", current, "\n"
+                recomlen2 = len(r.get_subreddit_recommendations(end, omit=None))
+                print "Your CURRENT Subreddit is:", current
+                print "Your NEW ENDING Subreddit is:", end, "\n"
 
             print "Your Choices of Subreddits To Pick Are:\n"
                 
@@ -75,9 +78,36 @@ class subredditGame:
                 print "Repicking CURRENT subreddit.\n"
                 current = str(r.get_random_subreddit())
                 choice = current
-            current = choice
-            path.append(choice)
-            numtimes += 1
+
+                current = choice
+                path.append(choice)
+                numtimes += 1
+
+            elif(choice.lower() == "hint"):
+                print "\nSubreddits related to", end, "are: "
+                endrecom = str(r.get_subreddit_recommendations(end, omit=None))
+
+                hold = ""
+                for i in endrecom:
+                    hold = hold + i
+
+                z = 0
+
+                while z < recomlen2 * 2:
+                    z += 1
+                    endrecom2 = hold.split("'")[z]
+                    print endrecom2
+                    z += 1
+
+                
+                path.append(choice)
+                numtimes += 1
+                
+            else :    
+                current = choice
+                path.append(choice)
+                numtimes += 1
+                
             if current.lower() == end.lower():
                 break
 
